0a86a99276d91e8a0e931da6736ab5c5ca074c47
Yugo Osano <yugo.osano@gmail.com>
2013-11-17 12:13:00 +0900

    first commit

---
+++ inotify_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# package 'inotify-tools' required
+
+# for inotify-tools, see:
+# http://ameblo.jp/itboy/entry-10172179452.html
+
+# monitor creation of files in the current directory
+# http://d.hatena.ne.jp/aki-yam/20090401/1238569171
+
+# '.' can be replaced by /home/yugo/cppmemo
+out_dir=. 
+
+events=(-e CREATE -e MODIFY)
+
+while inotifywait ${events[@]} $out_dir
+do
+    #-- sort file by time and then pick the latest --
+    newfile=`ls -rt | tail -n1`
+    echo "file created or modified: $newfile"
+done
+
+
