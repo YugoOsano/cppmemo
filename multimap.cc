9a54730303774201938f5269dbe33660dec796cf
YugoOsano <yugo.osano@gmail.com>
2014-11-30 16:04:19 +0900

    multimap.cc added.

---
+++ multimap.cc
@@ -0,0 +1,76 @@
+// Reference:
+// http://qiita.com/satoruhiga/items/957d69baceaaf68a8eb3
+
+#include <map>
+#include <vector>
+#include <iostream>
+
+struct Value{
+  int a;
+  int b;
+};
+
+class Property{
+public:
+  typedef std::multimap<int, Value>           MultiMapValue;
+  typedef std::multimap<int, Value>::iterator MultiMapValueIter;
+
+  void setValue(int id, Value value){
+    Map[id] = value;
+  }
+  Value getValue(int id){
+    return Map[id];
+  }
+  //-- for the multimap
+  void setValueMulti(int id, Value value){
+    MultiMap.insert(std::make_pair(id, value));
+  }
+  void getValueMulti(int id, std::vector<Value>& vec){
+    vec.clear();
+    
+    std::pair<MultiMapValueIter, MultiMapValueIter> range 
+      = MultiMap.equal_range(id);
+
+    for(MultiMapValueIter it = range.first; it != range.second; it++)
+      vec.push_back(it->second);
+  }
+
+protected:
+  std::map<int, Value> Map;
+  MultiMapValue        MultiMap;
+};
+
+int main()
+{
+  Property property;
+
+  Value v;
+
+  for(int i=0; i<10; i++){
+    v.a = 1000*i + 1;
+    v.b = 1000*i + 2;
+
+    property.setValue(i, v);
+
+    property.setValueMulti(0, v);
+  }
+
+  for(int i=0; i<10; i++){
+    std::cout << "v[" << i << "]: " 
+	      << property.getValue(i).a << ","   
+	      << property.getValue(i).b << std::endl;
+  }
+
+  std::vector<Value>           vecValue;
+ 
+  property.getValueMulti(0, vecValue);
+
+  for(std::vector<Value>::iterator it = vecValue.begin();
+      it != vecValue.end(); it++){
+    std::cout << "MultiMap v[0]: " 
+	      << (*it).a << "," 
+	      << (*it).b << std::endl;
+  }
+
+  return 0;
+}
