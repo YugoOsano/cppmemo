bbec39b9ec223638df6df768cd230b44e961037c
Yugo Osano <yugo.osano@gmail.com>
2018-03-10 16:23:15 +0900

    heap by c language

--- make_heap.cc
+++ make_heap.cc
@@ -5,18 +5,42 @@
 #include <algorithm>    // std::make_heap, std::pop_heap, std::push_heap, std::sort_heap
 #include <vector>       // std::vector
 
-/* max-heap property */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define HEAP_SIZE 10
+
+/* max-heap property 
+   https://www.codereading.com/algo_and_ds/ds/heap.html */
 void insert(int val, int heap[], int counter) {
   int i = counter + 1;
+
+  printf("insert called; counter: %d\ti: %d\n", counter, i);
 
   while ((i != 1) && (heap[i/2] < val)) {
 
     heap[i] = heap[i/2];
+    printf("value %d transfered from index %d to %d\n", heap[i], (i/2), i); 
     i = i/2;
   }
   heap[i] = val;
+  
 }
-
+void print_structure(int heap[], int start_index)
+{
+    for(int i = start_index; i < HEAP_SIZE; i++) {
+        printf("parent [%d]: %d\n", i, heap[i]);
+        /* left node */
+        if (i*2 < HEAP_SIZE) {
+            printf("\tchild L [%d]: %d\n", i*2, heap[i*2]);
+        }
+        /* right node */
+        if (i*2+1 < HEAP_SIZE) {
+            printf("\tchild R [%d]: %d\n", i*2+1, heap[i*2+1]);
+        }
+    }
+}
 
 int main () {
   int myints[] = {10,20,30,5,15};
@@ -55,17 +79,26 @@ int main () {
   for (unsigned i=0; i<v.size(); i++)
     std::cout << ' ' << v[i];
 
-  std::cout << '\n';
+  std::cout << "\n\n";
 
   //-----
-  int initial_int[] = {10,20,30,5,15};
-  int *heap;
-  for (int i=0; i<5; i++) {
-    insert(initial_int[i], heap, i+1);
+  
+  int heap[HEAP_SIZE];
+  int val = 0; /* insert data */
 
-    for (int j=0; j<=i; j++) std::cout << heap[j] << ' ';
-    std::cout << '\n';
-  } 
+  srand(getpid());
+
+  int myints10[] = {10,20,30,5,15,25,2,7,12,18};
+  
+  for(int i = 0; i < HEAP_SIZE; i++) {
+    /* random integer */
+    val = myints10[i];//rand();
+    
+    insert(val, heap, i);
+    printf("Insert [%d]: %d\n", i, val);
+  }
+  printf("\n");
+  print_structure(heap, 1);
   
   return 0;
 }
