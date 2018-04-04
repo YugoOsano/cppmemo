b03ca6db16a2c40831420618ea29705fd7cee322
Yugo Osano <yugo.osano@gmail.com>
2018-04-04 21:17:12 +0900

    haskell fizzbuzz created.

---
+++ haskellmemo/fizzbuzz.hs
@@ -0,0 +1,9 @@
+fizzbuzz :: Integer -> [String]
+fizzbuzz 0 = []
+fizzbuzz n
+    | n `mod` 15 == 0 = fizzbuzz(n - 1) ++ ["fizzbuzz"]
+    | n `mod` 3 == 0 = fizzbuzz(n - 1) ++ ["fizz"]
+    | n `mod` 5 == 0 = fizzbuzz(n - 1) ++ ["buzz"]
+fizzbuzz n = fizzbuzz(n - 1) ++ [show n]
+
+main = putStrLn "hello"
