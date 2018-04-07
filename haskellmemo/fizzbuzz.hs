30cace6e42312f33eabc14636ab5b21d643f411b
Yugo Osano <yugo.osano@gmail.com>
2018-04-07 11:24:33 +0900

    haskell fizzbuzz can be printed.

--- haskellmemo/fizzbuzz.hs
+++ haskellmemo/fizzbuzz.hs
@@ -6,4 +6,9 @@ fizzbuzz n
     | n `mod` 5 == 0 = fizzbuzz(n - 1) ++ ["buzz"]
 fizzbuzz n = fizzbuzz(n - 1) ++ [show n]
 
-main = putStrLn "hello"
+main :: IO ()
+main = do
+  putStrLn "hello"
+  print (fizzbuzz 100)
+  
+  
