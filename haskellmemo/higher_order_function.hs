7655ec187762ab0bb9d7359176c52221a94e9ccc
Yugo Osano <yugo.osano@gmail.com>
2018-10-23 22:50:31 +0900

    A comment was added to higher_order_fucntion.hs

--- haskellmemo/higher_order_function.hs
+++ haskellmemo/higher_order_function.hs
@@ -1,4 +1,5 @@
 main = do
   -- list comprehension
   -- https://sites.google.com/site/kiyohiko90091e/programming/haskell/gen-lists
+  -- [map-func x | x <- xs, filter-predicate]
   print [x | x <- [0 .. 100]]
