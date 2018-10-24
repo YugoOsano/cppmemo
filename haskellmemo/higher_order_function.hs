3540d13638c3e0a724e66fb003a9c0e2235f98d2
Yugo Osano <yugo.osano@gmail.com>
2018-10-24 21:18:03 +0900

    map, monad were added to higher_order_function.

--- haskellmemo/higher_order_function.hs
+++ haskellmemo/higher_order_function.hs
@@ -3,3 +3,18 @@ main = do
   -- https://sites.google.com/site/kiyohiko90091e/programming/haskell/gen-lists
   -- [map-func x | x <- xs, filter-predicate]
   print [x | x <- [0 .. 100]]
+
+  -- about map function
+  -- https://qiita.com/knknkn1162/items/92de8dd250ff94cd86f1
+  -- the following two are same
+  print  (map (+2) [0 .. 10]) 
+  print $ map (+2) [0 .. 10]
+
+  -- map with lambda
+  print $ map (\x -> x+2) [0 .. 10]
+
+  -- create a Maybe monad
+  -- https://qiita.com/7shi/items/c7d7eec066af0fe0688d
+  let a = Just 1
+      b = return 1 :: Maybe Int
+  print (a, b)
