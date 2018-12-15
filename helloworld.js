3d191a14b8f68e012cbb3a9a03414f8877ba3342
Yugo Osano <yugo.osano@gmail.com>
2018-12-15 19:14:41 +0900

    nodejs trial

---
+++ helloworld.js
@@ -0,0 +1,5 @@
+// to install:
+// https://qiita.com/seibe/items/36cef7df85fe2cefa3ea
+// to run: node helloworld.js
+// https://jsprimer.net/use-case/nodecli/helloworld/
+console.log("Hello, world!");
