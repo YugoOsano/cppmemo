53a5891f9ff31efc63e696273ee9a6418a3dbaf2
Yugo Osano <yugo.osano@gmail.com>
2019-01-01 17:31:22 +0000

    factorial function was added to fizzbuzz.hs as a typical recursive function.

--- haskellmemo/fizzbuzz.hs
+++ haskellmemo/fizzbuzz.hs
@@ -6,7 +6,15 @@ fizzbuzz n
     | n `mod` 5 == 0 = fizzbuzz(n - 1) ++ ["buzz"]
 fizzbuzz n = fizzbuzz(n - 1) ++ [show n]
 
--- the above vertical bars are refered to as guard
+{- the above vertical bars are refered to as guard
+   A Bool formula is followed after every guard
+-}
+
+-- a typical recursive function
+factorial :: Int -> Int
+factorial n
+    | n == 0 = 1
+factorial n = n * factorial (n-1)
 
 main :: IO ()
 main = do
@@ -14,5 +22,5 @@ main = do
   print (fizzbuzz 100)
   -- $ operator is right-associative
   print $ fizzbuzz 100
-  
+  print $ factorial 5  
   
