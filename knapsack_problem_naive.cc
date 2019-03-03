9b7a0a3fded3832c851782436227c67cde9495d9
Yugo Osano <yugo.osano@gmail.com>
2019-03-03 17:51:31 +0900

    backtrace monitoring added.

--- knapsack_problem_naive.cc
+++ knapsack_problem_naive.cc
@@ -1,7 +1,12 @@
 #include <algorithm>
 #include <stdio.h>
+#include <execinfo.h>
+
+//-- ref about execinfo:
+//  http://man7.org/linux/man-pages/man3/backtrace.3.html
 
 const int MAX_N = 10000;
+const int BT_BUF_SIZE = 100;
 
 int n = 4, W = 5;
 int w[MAX_N] = {2,1,3,2};
@@ -10,8 +15,11 @@ int v[MAX_N] = {3,2,4,2};
 //-- see stack by bt command in gdb
 //-- i th item
 int rec(int i, int remaining_weight_capacity) {
-  printf("%d th item picked; remaining capacity: %d\n",
-	 i, remaining_weight_capacity);
+
+  void *buffer[BT_BUF_SIZE];
+  int nptrs = backtrace(buffer, BT_BUF_SIZE);
+  printf("%d th item picked; remaining capacity: %d, bt: %d\n",
+	 i, remaining_weight_capacity, nptrs);
 
   if (i == n) {
     //-- no remaining
