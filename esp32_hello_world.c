98485415b0528cfb613c7f77ce88d5629a87a695
Yugo Osano <yugo.osano@gmail.com>
2019-03-31 11:32:12 +0900

    comment added about esp32.

--- esp32_hello_world.c
+++ esp32_hello_world.c
@@ -7,6 +7,9 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+/* debug tips with OpenOCD
+   https://qiita.com/rukihena/items/f5585436c16f74d0f01d
+*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
