5a7378278c3ae1f0da9d4e7377544f6414c99aee
Yugo Osano <yugo.osano@gmail.com>
2019-10-13 19:59:56 +0900

    dynamic planning storing

--- binary_search_like_combination.cc
+++ binary_search_like_combination.cc
@@ -1,38 +1,62 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <set>
+#include <map>
 
-void DumpCombination(
-         const std::vector<std::string>&          input_vector,
-	 const std::vector<std::string>::iterator iter,
-	 std::vector<std::string>&                temp_vector) {
+//--
+using Type = std::vector<std::string>;
+std::set<size_t> DumpCombination(
+         const Type&          input_vector,
+	 const Type::iterator iter,
+	 Type&                temp_vector,
+	 std::map<Type::iterator, std::set<size_t>>& sum_string_length) {
+  //-- all length set to return
+  std::set<size_t> to_return;
+  
   //-- dump when iter reaches the end of input
   if (iter == input_vector.cend()) {
     for (const std::string& elem : temp_vector)
       std::cout << elem << ", ";
     std::cout << std::endl;
-    return;
+    return to_return;
   }
   auto new_iter = iter + 1;
   //-- without appending the current iter's target
   std::vector<std::string> new_without(temp_vector);
   new_without.push_back("_");
-  DumpCombination(input_vector,
-		  new_iter,
-		  new_without);
+  const std::set<size_t>& to_add0 = DumpCombination(input_vector,
+						    new_iter,
+						    new_without,
+						    sum_string_length);
+  to_return.insert(to_add0.cbegin(),
+		   to_add0.cend());
   //-- appending the current iter's target
   std::vector<std::string> new_with(temp_vector);
   new_with.push_back(*iter);
-  DumpCombination(input_vector,
-		  new_iter,
-		  new_with);
+  const std::set<size_t>& to_add1 = DumpCombination(input_vector,
+						    new_iter,
+						    new_with,
+						    sum_string_length);
+  to_return.insert(to_add1.cbegin(),
+		   to_add1.cend());
+
+  std::set<size_t> to_add2;
+  for (const size_t elem : to_return)
+    to_add2.insert(elem + (*iter).length());
+  to_return.insert(to_add2.cbegin(),
+		   to_add2.cend());
+  return to_return;
 }
 
 int main() {
   std::vector<std::string> phonetic {"xray", "alpha", "romeo", "juliet"};
   std::vector<std::string> empty_vec{};
+  std::map<Type::iterator, std::set<size_t>> dp_recorder; 
+  
   DumpCombination(phonetic,
 		  phonetic.begin(),
-		  empty_vec);
+		  empty_vec,
+		  dp_recorder);
   return 0;
 }
