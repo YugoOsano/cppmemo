40f91e7cf3318ef8b3994adebbd867509fee462f
Yugo Osano <yugo.osano@gmail.com>
2019-12-25 21:31:48 +0900

    gdb was practiced with see_assembler_sample.c.

--- see_assembler_sample.c
+++ see_assembler_sample.c
@@ -1,5 +1,13 @@
 // intel2gas
 // https://seesaawiki.jp/w/yamaneko1144/d/GAS%a4%c8NASM%a4%c8NASK%a4%cb%a4%c4%a4%a4%a4%c6
+// Text User Interface mode can be used by (-q to skip initial message)
+// gdb --tui -q a.out
+
+// gdb) layout asm
+// to see assembler
+
+// gdb) layout prev
+// to go back c code
 
 void null() {
   return;
@@ -11,4 +19,11 @@ int return_zero() {
 
 int return_one() {
   return 1;
+}
+
+int main () {
+  null();
+  return_zero();
+  return_one();
+  return 0;
 }
