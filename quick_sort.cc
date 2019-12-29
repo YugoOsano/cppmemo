66f4655a37f8adaddd49c4c4cdf431e326721c15
Yugo Osano <yugo.osano@gmail.com>
2019-12-29 10:43:35 +0900

    no copy version of quick sort was created.

--- quick_sort.cc
+++ quick_sort.cc
@@ -47,6 +47,45 @@ std::list<T> QuickSortFunctional(const std::list<T>& list,
 				    sorted_list);
   return std::move(sorted_list);
 }
+//-- wrapper class to see no copy happen
+//   ref: https://stackoverflow.com/questions/19826376/insert-into-vector-having-objects-without-copy-constructor
+template <typename T>
+struct SortedList {
+  SortedList()=default;
+  SortedList(const SortedList&)=delete;
+  SortedList(SortedList&&) noexcept =default;
+  SortedList& operator=(SortedList&&) noexcept=default;
+  std::list<T> list_;
+};
+template <typename T>
+SortedList<T> QuickSortWrappedClass(
+		const std::list<T>& list,
+		SortedList<T>&      sorted_list) {
+  if (list.size() == 0) return std::move(sorted_list);
+  if (list.size() == 1) {
+    sorted_list.list_.push_back(list.front());
+    return std::move(sorted_list);
+  }
+  using IterType = typename std::list<T>::const_iterator;
+  const std::pair<IterType, IterType>& minmax
+              = std::minmax_element(list.cbegin(),
+				    list.cend());
+  const T middle = (*minmax.first + *minmax.second) / 2;
+  std::list<T> upper_list, lower_list;
+
+  for (const T element : list) {
+    if (element > middle) upper_list.push_back(element);
+    else                  lower_list.push_back(element);
+  }
+  SortedList<T> sorted_list_update0
+              = QuickSortWrappedClass(upper_list,
+				      sorted_list);
+  SortedList<T> sorted_list_update1
+              = QuickSortWrappedClass(lower_list,
+				      sorted_list_update0);
+  return std::move(sorted_list_update1);
+}
+
 int main () {
   const std::list<int> list{-100, -300, 987, -567, 100, 256, -1298, 675};
   std::list<int> sorted_list;
@@ -54,7 +93,14 @@ int main () {
 
   for (const int element : sorted_list)
     std::cout << element << std::endl;
-  
+
+  SortedList<int>        sorted_list_initial;
+  const SortedList<int>& sorted_list_wrapped
+     = QuickSortWrappedClass(list, sorted_list_initial);
+
+  for (const int element : sorted_list_wrapped.list_)
+    std::cout << element << std::endl;
+
   return 0;
 }
 
