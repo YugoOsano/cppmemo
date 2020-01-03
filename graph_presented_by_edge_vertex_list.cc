94008a6277f0a75afab47876205505af58e8e36f
Yugo Osano <yugo.osano@gmail.com>
2020-01-03 09:17:12 +0800

    Strongly typed native type is created in graph practice.

--- graph_presented_by_edge_vertex_list.cc
+++ graph_presented_by_edge_vertex_list.cc
@@ -15,18 +15,33 @@ struct Edge {
   const int to_;
 };
 
-//-- see StackOverFlow (C++ derive from native type)
+//-- see StackOverFlow (C++ derive from native type) + use of tag type
 //https://stackoverflow.com/questions/1183554/c-derive-from-a-native-type
-template <class T>
-class VertexToFirstEdgeNo {
+template <class T, class Tag = T>
+class TypeWrapper {
 public:
-  explicit VertexToFirstEdgeNo(const T value) :
+  explicit TypeWrapper(const T value) :
     value_(value) {}
-  operator T() {return value_;}
-  void operator= (const T x) {value_ = x;}
+  virtual ~TypeWrapper()=default;
+  operator T() const {return value_;}
+  void Set(const T& value) {value_ = value;}
+  void operator= (const TypeWrapper<T,Tag>& x) {value_ = x.value_;}
 private:
   T value_;
 };
+//-- tag types
+struct TagA{};struct TagB{};
+
+using VertexToFirstEdgeNo = TypeWrapper<int, TagA>;
+using EdgeToNextEdgeNo    = TypeWrapper<int, TagB>;
+
+//-- dummy function to confirm type incompatibility
+void F() {
+  VertexToFirstEdgeNo a(5),b(10);
+  a=b;
+  EdgeToNextEdgeNo c(20);
+  //c=a;//<- this will be errors about type (successfully)
+}
 
 int main () {
   std::vector<Edge> edge_list {
@@ -36,19 +51,20 @@ int main () {
     Edge(3,2), Edge(3,4)};
   const int vertex_maximum = 5;
   
-  std::vector<VertexToFirstEdgeNo<int>>
+  std::vector<VertexToFirstEdgeNo>
     vertex_to_first_edge_number(vertex_maximum + 1,
-				VertexToFirstEdgeNo<int>(-1));
-  for (size_t i_edge = 0;
+				VertexToFirstEdgeNo(-1));
+  for (int i_edge = 0;
        i_edge < edge_list.size(); i_edge++) {
     const int from = edge_list.at(i_edge).from_;
-    if (vertex_to_first_edge_number.at(from) == -1)
-      vertex_to_first_edge_number.at(from) = i_edge;
+    if (vertex_to_first_edge_number.at(from) == -1) {
+      vertex_to_first_edge_number.at(from).Set(i_edge);
+    }
   }
   
-  VertexToFirstEdgeNo<int> a(123);
+  VertexToFirstEdgeNo a(123);
   std::cout << a << std::endl;
-  a = 246;
+  a.Set(246);
   std::cout << a << std::endl;
   return 0;
 }
