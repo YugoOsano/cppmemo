1be3b38d7fe6fee814ca5c04c8bc493fea4de9cf
Yugo Osano <yugo.osano@gmail.com>
2020-01-25 12:23:01 +0900

    commented on cryptopp_hello.

--- cryptopp_hello.cc
+++ cryptopp_hello.cc
@@ -9,15 +9,17 @@
 #include <cryptopp/sha.h>
 #include <cryptopp/hex.h>
 
+//-- Modern C++ Challenge Q.91
 std::string GetHash(const std::string& password) {
   CryptoPP::SHA512 sha;
   byte digest[CryptoPP::SHA512::DIGESTSIZE];
 
   sha.CalculateDigest(
-      digest,
-      reinterpret_cast<byte const*>(password.c_str()),
-      password.length());
-  
+      digest,                                         //output buffer for hashed string
+      reinterpret_cast<byte const*>(password.c_str()),//input string
+      password.length());                             // string length
+
+  //-- encode converts byte array to readable characters
   CryptoPP::HexEncoder encoder;
   std::string result;
   encoder.Attach(new CryptoPP::StringSink(result));
