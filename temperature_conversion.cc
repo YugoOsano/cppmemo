dc066b4595f97930b5538c316936a18d4f8b1c26
Yugo Osano <yugo.osano@gmail.com>
2020-01-30 20:39:29 +0900

    temperature conversion is ready.

--- temperature_conversion.cc
+++ temperature_conversion.cc
@@ -1,8 +1,8 @@
 namespace temperature {
   enum class Scale {
-		    Celsius,
-		    Fahrenheit,
-		    Kelvin
+	Celsius,
+	Fahrenheit,
+	Kelvin
   };
   template <Scale S>
   class Quantity {
@@ -13,8 +13,42 @@ namespace temperature {
   private:
     const double amount_;
   };  
+  //-- conversion
+  template <Scale S, Scale R>
+  struct ConversionTraits {
+    static double Convert(const double value)=delete;
+  };
+  template <>
+  struct ConversionTraits<Scale::Celsius, Scale::Fahrenheit> {
+    static double Convert(const double value) {
+      return (value * 9) / 5 + 32;
+    }
+  };
+  template <>
+  struct ConversionTraits<Scale::Fahrenheit, Scale::Celsius> {
+    static double Convert(const double value) {
+      return (value - 32) * 5 / 9;
+    }
+  };
+  template <Scale R, Scale S>
+  constexpr Quantity<R> temperature_cast(const Quantity<S> q) {
+    return Quantity<R>(ConversionTraits<S,R>::Convert(
+	static_cast<double>(q)));
+  }
 } // end of temperature
+namespace temperature::scale_literal {
+  constexpr Quantity<Scale::Celsius> operator "" _deg
+          (const long double amount) {
+    return Quantity<Scale::Celsius> {static_cast<double>(amount)};
+  }
+} // end of scale_literal
 
 int main () {
+  // for all enum, functions, structs
+  using namespace temperature;
+  //-- using for _deg
+  using namespace temperature::scale_literal;
+  auto t1{ 36.5_deg };
+  auto tf = temperature_cast<Scale::Fahrenheit>(t1);
   return 0;
 }
