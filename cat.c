53dbeddbb5fb4c067422cf76131a44f96e0977d0
Yugo Osano <yugo.osano@gmail.com>
2020-02-01 11:46:17 +0900

    cat.c was completed.

--- cat.c
+++ cat.c
@@ -1,12 +1,28 @@
 /* Normal Linux Programming p-86
    handmade cat command */
 
+#include <stdio.h>
+#include <stdlib.h>
+/* close is included in unistd.h */
+#include <unistd.h>
+
 /* open is included in fcntl.h */
 #include <fcntl.h>
+
+static void do_cat(const char* path);
+static void die(   const char* s);
 
 #define BUFFER_SIZE 2048
 
 int main (int argc, char* argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "%s: file not given\n", argv[0]);
+    exit(1);
+  }
+  for(int i = 1; i < argc; i++) {
+    do_cat(argv[i]);
+  }
+  exit(0);
   return 0;
 }
 
@@ -17,4 +33,29 @@ do_cat (const char *path) {
   int n;
 
   fd = open(path, O_RDONLY);
+  if (fd < 0) die(path);
+  for (;;) {
+    n = read(fd, buf, sizeof buf);
+    if (n < 0) die(path);
+    if (n == 0) break;
+    if (write(STDOUT_FILENO, buf, n) < 0) die(path);
+  }
+  if (close(fd) < 0) die(path);
 }
+static void die(const char* s) {
+  /* this is stdio.h; p stands for print */
+  perror(s);
+  exit(1);   /* stdlib.h */
+}
+/*
+  StackOverFlow 62936
+  What does the number in parentheses shown after Unix command names in manpages mean?
+  1 General commands
+  2 System calls
+  3 C library functions
+  4 Special files (usually devices, those found in /dev) and drivers
+  5 File formats and conventions
+  6 Games and screensavers
+  7 Miscellanea
+  8 System administration commands and daemons
+ */
