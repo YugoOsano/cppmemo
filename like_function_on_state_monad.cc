4ab2e790c107fac2563f4f3e3256dfb54119c16d
Yugo Osano <yugo.osano@gmail.com>
2020-03-07 22:51:25 +0900

    memo on GDB appended.

--- like_function_on_state_monad.cc
+++ like_function_on_state_monad.cc
@@ -15,4 +15,22 @@ int main() {
   return 0; //-- 0x7ffffffee3a7
 }
 
+/*
+GDB memo
+Reload source file (to switch debugging target): simple use of
+directory command
+https://stackoverflow.com/questions/4118207/how-to-reload-source-files-in-gdb
+
+variable printing in GDB:
+info variables : static/global
+info locals
+info args
+https://stackoverflow.com/questions/6261392/printing-all-global-variables-local-variables
+
+info frame : to show stack frame
+https://stackoverflow.com/questions/7848771/how-can-one-see-content-of-stack-with-gdb
+
+info proc mappings : to show mapped memory
+https://stackoverflow.com/questions/5691193/gdb-listing-all-mapped-memory-regions-for-a-crashed-process
+ */
 
