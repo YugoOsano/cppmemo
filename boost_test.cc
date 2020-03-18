748c62f94437c49e90e5133c0767af6448e2d75d
Yugo Osano <yugo.osano@gmail.com>
2020-03-18 11:09:34 +0900

    practiced to extract keys from a map by boost range tool.

--- boost_test.cc
+++ boost_test.cc
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <boost/range/algorithm.hpp>
 
+#include <map>
+#include <set>
+#include <boost/range/adaptor/map.hpp>
+#include <boost/range/algorithm/copy.hpp>
+
 void output(const int a) {
   std::cout << a << std::endl;
 }
@@ -16,5 +21,17 @@ int main () {
     std::cout << a << std::endl;
   };
   boost::for_each(alist, outputlambda);
+
+  // extract maps keys to store to std::set
+  // https://stackoverflow.com/questions/110157/how-to-retrieve-all-keys-or-values-from-a-stdmap-and-put-them-into-a-vector
+  {
+    const std::map<int, int> mapint{{0,1},{2,3},{4,5}};
+    std::set<int> keys;
+    boost::copy(mapint | boost::adaptors::map_keys,
+		std::inserter(keys, keys.begin()));
+    for (const int key : keys)
+      std::cout << key << " ";
+    std::cout << std::endl;
+  }
   return 0;
 }
