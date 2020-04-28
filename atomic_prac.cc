f9f23c9d4c389a72f434a8a48bf5d9927e7b82cb
Yugo Osano <yugo.osano@gmail.com>
2020-04-28 06:56:27 +0000

    memo about memory model was added to atomic_prac.cc.

--- atomic_prac.cc
+++ atomic_prac.cc
@@ -28,3 +28,18 @@ int main () {
   
   return 0;
 }
+/*
+memory model coming with using std::atomic is well presented at:
+https://stackoverflow.com/questions/6319146/c11-introduced-a-standardized-memory-model-what-does-it-mean-and-how-is-it-g
+
+-> If the system requires atomicity but not ordering
+  ((x,y)=(0,37) can be accepted below), 'memory_order_relaxed' works well
+  to secure performance.
+
+Global
+atomic<int> x, y;
+
+Thread 1                            Thread 2
+x.store(17,memory_order_relaxed);   cout << y.load(memory_order_relaxed) << " ";
+y.store(37,memory_order_relaxed);   cout << x.load(memory_order_relaxed) << endl;
+*/
