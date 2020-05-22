faaf8b2b24fc4feb0949f1679156badb25e206bf
Yugo Osano <yugo.osano@gmail.com>
2020-05-22 10:13:14 +0900

    the way to take values from a multimap was verified.

--- multimap.cc
+++ multimap.cc
@@ -34,7 +34,8 @@ public:
     for(MultiMapValueIter it = range.first; it != range.second; it++)
       vec.push_back(it->second);
   }
-
+  //-- simple getter --
+  const MultiMapValue& GetMultiMap() const {return MultiMap;}
 protected:
   std::map<int, Value> Map;
   MultiMapValue        MultiMap;
@@ -51,6 +52,8 @@ int main()
     v.b = 1000*i + 2;
 
     property.setValue(i, v);
+    //-- coming of the same key will replace the existing key-value set
+    if (i==5) property.setValue(i, Value{-1,-1});
 
     property.setValueMulti(0, v);
   }
@@ -60,11 +63,8 @@ int main()
 	      << property.getValue(i).a << ","   
 	      << property.getValue(i).b << std::endl;
   }
-
   std::vector<Value>           vecValue;
- 
   property.getValueMulti(0, vecValue);
-
   for(std::vector<Value>::iterator it = vecValue.begin();
       it != vecValue.end(); it++){
     std::cout << "MultiMap v[0]: " 
@@ -72,5 +72,15 @@ int main()
 	      << (*it).b << std::endl;
   }
 
+  const std::pair<std::multimap<int, Value>::const_iterator,
+		  std::multimap<int, Value>::const_iterator>&
+    value_range = property.GetMultiMap().equal_range(0);
+  std::cout << "\nloop over iterator taken by equal_range(0):" << std::endl;
+  for (auto iter = value_range.first;
+       iter     != value_range.second; iter++) {
+    std::cout << iter->first << ": "
+	      << iter->second.a << ", "
+	      << iter->second.b << std::endl;
+  }
   return 0;
 }
