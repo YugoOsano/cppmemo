a5a47a1510c22e33d5b9b65179531259d903ad5d
Yugo Osano <yugo.osano@gmail.com>
2020-05-26 13:23:39 +0900

    optional for file stream was practiced.

--- optional.cc
+++ optional.cc
@@ -5,6 +5,8 @@
 #include <optional>
 #include <iostream>
 #include <boost/optional.hpp>
+#include <fstream>
+#include <string>
 
 int main() {
   std::optional<int> x(std::nullopt_t);
@@ -15,6 +17,18 @@ int main() {
   
   boost::optional<int> maybe_x = boost::none;
 
+  //-- optional for file stream
+  const bool is_output = false;//true;
+  boost::optional<std::ofstream> maybe_ofs =
+    [](const bool is_out)->boost::optional<std::ofstream>
+    {
+     if (!is_out) return boost::none;
+     return std::move(std::ofstream("output.dat"));
+    }(is_output);
+
+  if (maybe_ofs != boost::none) {
+    maybe_ofs.get() << "hello maybe ofs!" << std::endl;
+  }
   return 0;
 }
 
