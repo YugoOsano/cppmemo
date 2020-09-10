8929fd4047bd250315d9173819af6257ca7cfda1
Yugo Osano <yugo.osano@gmail.com>
2020-09-10 22:54:17 +0900

    concat_iterator now can be run.

--- concat_iterator.cc
+++ concat_iterator.cc
@@ -2,48 +2,49 @@
 #include <set>
 #include <vector>
 #include <utility>
+#include <iostream>
 
 class ConcatIterator {
 public:
   ConcatIterator() :
     iter_pair_list_({}),
-    begin_(nullptr),
-    end_(nullptr) {}
+    begin_(nullptr) {}
+  //    end_(nullptr) {}
   void PushBack(const std::set<size_t>::const_iterator begin,
 		const std::set<size_t>::const_iterator end) {
-    iter_pair_list_.push_back(std::make_pair(begin, end));
     if (iter_pair_list_.size() == 0) {
       begin_ = begin;
     }
-    end_ = end;
+    //    end_ = end;
     iter_pair_list_.push_back(std::make_pair(begin, end));
   }
   void Initialize() {
     current_       = begin_;
     current_range_ = iter_pair_list_.cbegin();
   }
-  bool Forward() {
-    if (current_ != current_range_->second) {
-      current_ = std::next(current_);
-      return true;
+  void Forward() {
+    if (current_ == iter_pair_list_.crbegin()->second) return;
+    if (current_ == current_range_->second) {
+      current_range_++;
+      current_ = current_range_->first;
+      return;
     }
-    if (std::next(current_range_) == iter_pair_list_.cend())
-      return false;
-    current_range_++;
-    current_ = current_range_->first;
+    current_++;
   }
-  const std::set<size_t>::const_iterator& Begin() const {
-    return begin_;
-  }
+  // const std::set<size_t>::const_iterator& Begin() const {
+  //   return begin_;
+  // }
   const std::set<size_t>::const_iterator& End() const {
-    return end_;
+    return iter_pair_list_.crbegin()->second;
   }
-  
+  const std::set<size_t>::const_iterator& Current() const {
+    return current_;
+  }
 private:
   std::vector<std::pair<std::set<size_t>::const_iterator,
 			std::set<size_t>::const_iterator>> iter_pair_list_;
   std::set<size_t>::const_iterator begin_;
-  std::set<size_t>::const_iterator end_;
+  //  std::set<size_t>::const_iterator end_;
 
   std::set<size_t>::const_iterator          current_;
   decltype(iter_pair_list_)::const_iterator current_range_;
@@ -51,12 +52,17 @@ private:
 
 int main() {
   std::set<size_t> example {0,1,2,3,4,5};
+  std::set<size_t> example2 {10,11,12,13,14,15};
+
   ConcatIterator coniter;
   coniter.PushBack(example.cbegin(),
 		   example.cend());
-  coniter.Initialize();
-  // for (auto iter = coniter.Begin();
-  //      iter     != coniter.End(); iter.Forward()) {
-  // // }
+  coniter.PushBack(example2.cbegin(),
+		   example2.cend());
+  for (coniter.Initialize() ;coniter.Current() != coniter.End();
+       coniter.Forward()) {
+    std::cout << *coniter.Current() << ", ";
+  }
+  std::cout << std::endl;
   return 0;
 }
