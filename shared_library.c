8b441efa34d2fcaa6bed98fc39b1823fbe5daf80
Yugo Osano <yugo.osano@gmail.com>
2021-03-05 16:34:20 +0900

    memo on shared lib.

--- shared_library.c
+++ shared_library.c
@@ -2,6 +2,13 @@
 // transcribed from https://taiyakisun.hatenablog.com/entry/20150506/1430896155
 // compile:
 // gcc -shared -fPIC -o libsharedlib.so shared_library.c
+// (of course g++ works as well; no difference in object)
+// g++ -shared -fPIC -o libsharedlib.so shared_library.c
+
+// 1) declaration without static let the library public in gcc
+// 2) the file name has to start with lib***.so
+// 3) PIC stands for Position Independent Code (recommended)
+//    which means it runs regardless of its absolute address.
 
 int add (int a, int b) {
   return a+b;
