eff07eb1de863a9fd6ab07a7dc624c6f0993ddce
Yugo Osano <yugo.osano@gmail.com>
2022-08-18 15:18:47 +0900

    CTest was practiced.

--- cmake_example/main.cpp
+++ cmake_example/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <cassert>
 #include "func.hpp"
 
 int main() {
   std::cout << "Hello, CMake!" << std::endl;
   func();
+
+  // assert statements are for ctest
+  assert(true);
+  // assert(false); // ctest will end with fail
   return 0;
 }
